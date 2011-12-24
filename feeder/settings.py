54d9f572693e9fcc981d4038c8ed1fb13da2037e
Dan Davison <davison@counsyl.com>
2011-12-23 18:55:05 -0800

    Add a jquery timepicker

--- feeder/settings.py
+++ feeder/settings.py
@@ -82,6 +82,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_DIRECTORY, 'thirdparty'),
 )
 
 # List of finder classes that know how to find static files in
@@ -125,7 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     # 'django.contrib.sites',
     # 'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
