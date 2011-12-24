32c84eb76dea5b7212ed0aa02706d03a5b801633
Dan Davison <davison@counsyl.com>
2011-12-24 17:13:39 +0000

    Rearrange view code

--- feeder/words/views.py
+++ feeder/words/views.py
@@ -18,6 +18,62 @@ from words.models import Item, Entry, Feed
 MAX_N_WORDSETS = 1000
 
 
+def home(request):
+    if request.method == 'POST':
+        form = BrowseForm(request.POST)
+        if form.is_valid():
+            start_time = get_datetime_from_time_today(
+                form.cleaned_data['start_time'])
+            end_time = get_datetime_from_time_today(
+                form.cleaned_data['end_time'])
+            return frequent_wordsets(start_time, end_time)
+    else:
+        today = datetime.today()
+        now = datetime.now()
+        form = BrowseForm(initial={'start_date': today,
+                                   'end_date': today,
+                                   'end_time': now})
+
+    entry_pub_times = sorted(Entry.objects.values_list('pub_time', flat=True))
+    earliest, latest = entry_pub_times[0], entry_pub_times[-1]
+    n_entries = len(entry_pub_times)
+
+    return render_to_response('browse.html', {
+        'title': 'Feed Reader',
+        'n_items': Item.objects.count(),
+        'n_entries': n_entries,
+        'n_feeds': Feed.objects.count(),        
+        'earliest': earliest,
+        'latest': latest,
+        'now': now,
+        'form': form,
+    }, context_instance=RequestContext(request))
+
+
+class BrowseForm(forms.Form):
+    start_date = forms.DateField(
+        label='Start date',
+        required=True,
+        widget=forms.DateInput(
+            attrs={
+                'class': 'required, datepicker'}))
+    start_time = forms.TimeField(
+        label = 'Start time',
+        required=True,
+        widget=forms.TimeInput(
+            attrs={'class': 'required, timepicker'}))
+    end_date = forms.DateField(
+        label='End date',
+        required=True,
+        widget=forms.DateInput(
+            attrs={
+                'class': 'required, datepicker'}))
+    end_time = forms.TimeField(
+        required=True,
+        widget=forms.TimeInput(
+            attrs={'class': 'required, timepicker'}))
+
+
 def frequent_wordsets(start_time, end_time):
     executable = os.path.join(settings.SITE_DIRECTORY,
                               '../bin/apriori')
@@ -62,61 +118,3 @@ def matching_items(request):
 
     return render_to_response('items.html',
                               {'items': items})
-
-
-
-class BrowseForm(forms.Form):
-    start_date = forms.DateField(
-        label='Start date',
-        required=True,
-        widget=forms.DateInput(
-            attrs={
-                'class': 'required, datepicker'}))
-    start_time = forms.TimeField(
-        label = 'Start time',
-        required=True,
-        widget=forms.TimeInput(
-            attrs={'class': 'required, timepicker'}))
-    end_date = forms.DateField(
-        label='End date',
-        required=True,
-        widget=forms.DateInput(
-            attrs={
-                'class': 'required, datepicker'}))
-    end_time = forms.TimeField(
-        required=True,
-        widget=forms.TimeInput(
-            attrs={'class': 'required, timepicker'}))
-
-
-def home(request):
-    if request.method == 'POST':
-        form = BrowseForm(request.POST)
-        if form.is_valid():
-            start_time = get_datetime_from_time_today(
-                form.cleaned_data['start_time'])
-            end_time = get_datetime_from_time_today(
-                form.cleaned_data['end_time'])
-            return frequent_wordsets(start_time, end_time)
-    else:
-        today = datetime.today()
-        now = datetime.now()
-        form = BrowseForm(initial={'start_date': today,
-                                   'end_date': today,
-                                   'end_time': now})
-
-
-    entry_pub_times = sorted(Entry.objects.values_list('pub_time', flat=True))
-    earliest, latest = entry_pub_times[0], entry_pub_times[-1]
-    n_entries = len(entry_pub_times)
-
-    return render_to_response('browse.html', {
-        'title': 'Feed Reader',
-        'n_items': Item.objects.count(),
-        'n_entries': n_entries,
-        'n_feeds': Feed.objects.count(),        
-        'earliest': earliest,
-        'latest': latest,
-        'now': now,
-        'form': form,
-    }, context_instance=RequestContext(request))
