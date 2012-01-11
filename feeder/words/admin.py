f83194e7e8b11d1e7a518cd194b0c5a9b3d4ee0d
Dan Davison <davison@counsyl.com>
2012-01-10 17:58:21 -0800

    Better name: url -> link

--- feeder/words/admin.py
+++ feeder/words/admin.py
@@ -16,11 +16,11 @@ class ItemAdmin(admin.ModelAdmin):
     def entry__pub_time(self, obj):
         return obj.entry.pub_time
 
-    def entry__feed__url(self, obj):
+    def entry__feed__link(self, obj):
         return '<a href=%s>%s</a>' % ((obj.entry.feed.url,) * 2)
-    entry__feed__url.allow_tags = True
+    entry__feed__link.allow_tags = True
 
-    list_display = ('entry__feed__url', 'entry__pub_time', 'truncated_value',)
+    list_display = ('entry__feed__link', 'entry__pub_time', 'truncated_value',)
     list_filter = ('entry__pub_time',)
     ordering = ('-entry__pub_time',)
     search_fields = ('value',)
